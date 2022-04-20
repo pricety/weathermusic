06b583b767acc4e85113de5edcdea20f89de1b15
Lauren <itslaurenhuynh@gmail.com>
2022-04-20 03:19:20 -0400

    added fahrenheit and celsius metric options and icons

--- weather.py
+++ weather.py
@@ -4,19 +4,28 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-def weather_info(location):
+def weather_info(location, metric_type):
     WEATHER_URL = "http://api.weatherstack.com/current"
 
     params = {
         "access_key": os.getenv("WEATHER_KEY"),
         "query": location,
-        "units": "f", #units in fahrenheit, will later add a button to switch between celcius and fahrenheit
+        "units": metric_type, #units in fahrenheit, will later add a button to switch between celcius and fahrenheit
     }
 
     weather_results = requests.get(WEATHER_URL, params)
     weather_data = weather_results.json()
     current_data = weather_data["current"] 
     location_data = weather_data["location"]
+
+    metric = ""
+    speed = ""
+    if metric_type == "m":
+        metric = "C"
+        speed = "km/h"
+    elif metric_type == "f":
+        metric = "F"
+        speed = "mi/h"
 
     weather_details = {
         "temperature": current_data["temperature"],
@@ -28,16 +37,16 @@ def weather_info(location):
         "cloud_cover": current_data["cloudcover"],
         "feels_like": current_data["feelslike"],
         "uv_index": current_data["uv_index"],
-        "visibility": current_data["visibility"]
+        "visibility": current_data["visibility"],
+        "metric_type": metric,
+        "speed": speed
     }
     
     location_details = {
         "name": location_data["name"],
         "region": location_data["region"],
-        "country": location_data["country"],
         "lat": location_data["lat"],
         "lon": location_data["lon"],
-        "local_time": location_data["localtime"]
     }
 
     return weather_details, location_details
